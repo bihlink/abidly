eb8d1a7fff877eed5e392253ec09e59199c5f79e
bihlink <95501288+bihlink@users.noreply.github.com>
2022-12-01 21:29:33 +0100

    Create navbar.php

---
+++ inc/navbar.php
@@ -0,0 +1,20 @@
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="https://abid.vercel.app/">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="https://abid.vercel.app/directory/">Directory</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="https://seolads.com/service/2-hq-high-authority-backlinks-fast-indexing">Add Link Here</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+    </nav>
