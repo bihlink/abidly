328723b3a8c9e9463d7ce5ebda5a2f8fa2a4249c
bihlink <95501288+bihlink@users.noreply.github.com>
2022-12-01 21:47:39 +0100

    Create pleroma.php

---
+++ pleroma.php
@@ -0,0 +1,19 @@
+<html lang="en">
+<head>
+    <title>Abid's Pleroma Instance</title>
+<?php
+  include_once('inc/inhead.php');   
+?>
+</head>
+<body class="text-white">
+<?php
+ include_once('inc/navbar.php');  
+?>
+    
+  <div class="container mt-3">
+    <?php
+    echo file_get_contents("https://maplefeed.bihlink.com/apiv2/feed?userurl=https://abid.cc/users/abid&theme=modern-light-min&size=100&header=true&replies=true&boosts=true");
+    ?>
+  </div>
+</body>
+</html>
