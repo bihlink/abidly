5eebdb34a4a1c6c55aeb8e3659485cb011d2adce
bihlink <95501288+bihlink@users.noreply.github.com>
2022-12-01 21:33:46 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,38 +1,14 @@
 <html lang="en">
 <head>
     <title>Abid's Network</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta http-equiv="x-ua-compatible" content="ie=edge,chrome=1">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.0/font/bootstrap-icons.css">
-    <style>
-      body {background-color: #3d3d3d;}
-    </style>
+<?php
+  include_once('./inc/inhead.php');   
+?>
 </head>
 <body class="text-white">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarNavDropdown">
-            <ul class="navbar-nav">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="https://abid.vercel.app/">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="https://abid.vercel.app/directory/">Directory</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="https://seolads.com/service/2-hq-high-authority-backlinks-fast-indexing">Add Link Here</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-    </nav>
+<?php
+ include_once('./inc/navbar.php');  
+?>
     
   <div class="container-fluid mt-3">
     <div class="row row-cols-2 row-cols-md-6 g-2">
