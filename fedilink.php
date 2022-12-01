c10de38053be59b6db6887cfd0209a916aa83d45
bihlink <95501288+bihlink@users.noreply.github.com>
2022-12-01 22:53:00 +0100

    Update fedilink.php

--- fedilink.php
+++ fedilink.php
@@ -15,5 +15,9 @@
     echo file_get_contents("https://maplefeed.bihlink.com/apiv2/feed?userurl=https://mastodon.world/users/fedilink&theme=modern-light-min&size=100&header=true&replies=true&boosts=true");
     ?>
   </div>
+  
+  <div class="my-3 p-3 text-center">
+      Feed Powered By <a href="https://maplefeed.bihlink.com" target="_blank">MaPleFeed</a>
+  </div>
 </body>
 </html>
