ddab560761f509612d0479b7085fca2a4aa3d5bf
bihlink <95501288+bihlink@users.noreply.github.com>
2026-05-22 12:53:38 +0200

    Add shadow effect to container div

--- directory/index.php
+++ directory/index.php
@@ -13,7 +13,7 @@
     </style>
 </head>
 <body>
-<div class="container">
+<div class="container shadow mt-3">
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
