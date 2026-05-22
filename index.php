b4fdaa03db04c6ed9e28095875a7840925c3b0fa
bihlink <95501288+bihlink@users.noreply.github.com>
2026-05-22 13:08:06 +0200

    Update blog title and add new blog link

--- index.php
+++ index.php
@@ -14,11 +14,12 @@
 
         <div class="row clearfix">
             <div class="col">
-                <h3>Blog</h3>
+                <h3>Abid's Network Blog</h3>
                 <ul class="list-group list-group-flush">
                   <li class="list-group-item"><a href="https://abid.vercel.app/blog/?p=27">How To Get A Youtube Short To Go Viral</a></li>
                   <li class="list-group-item"><a href="https://abid.vercel.app/blog/?p=28">Unlocking Opportunities: How to Earn Money Online as a Web Developer and Web Designer</a></li>
                   <li class="list-group-item"><a href="https://abid.vercel.app/blog/?p=29">Undoing the Belief in Linear Time: Entering the Holy Instant</a></li>
+                  <li class="list-group-item"><a href="https://abid.vercel.app/blog/?p=30">Are Changes Required in the Life Situation of God’s Teachers?</a></li> 
                 </ul>
             </div>
         </div>
