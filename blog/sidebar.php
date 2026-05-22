c839d03ac9dcd4c64000d850f56c494077a15cc6
bihlink <95501288+bihlink@users.noreply.github.com>
2026-05-22 12:46:32 +0200

    Update sidebar with external links
    Added a list of external links to the sidebar.

--- blog/sidebar.php
+++ blog/sidebar.php
@@ -1,1 +1,11 @@
-<h4>Sidebar</h4>
+<h4>Links</h4>
+<ul class="list-group list-group-flush">
+    <li class="list-group-item"><a href="https://linktr.ee/anwebservices" target="_blank" class="text-decoration-none">https://linktr.ee/anwebservices</a></li>
+    <li class="list-group-item"><a href="https://linktr.ee/tubelinks" target="_blank" class="text-decoration-none">https://linktr.ee/tubelinks</a></li>
+    <li class="list-group-item"><a href="https://linktr.ee/topbacklinks" target="_blank" class="text-decoration-none">https://linktr.ee/topbacklinks</a></li>
+    <li class="list-group-item"><a href="https://linktr.ee/seolads" target="_blank" class="text-decoration-none">https://linktr.ee/seolads</a></li>
+    <li class="list-group-item"><a href="https://linktr.ee/bihlink" target="_blank" class="text-decoration-none">https://linktr.ee/bihlink</a></li>
+    <li class="list-group-item"><a href="https://linktr.ee/tubyme" target="_blank" class="text-decoration-none">https://linktr.ee/tubyme</a></li>
+    <li class="list-group-item"><a href="https://bihlink.carrd.co/" target="_blank" class="text-decoration-none">https://bihlink.carrd.co/</a></li>
+    <li class="list-group-item"><a href="https://toplinks.carrd.co/" target="_blank" class="text-decoration-none">https://toplinks.carrd.co/</a></li>
+</ul>
