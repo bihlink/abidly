fe5f8b6cee27c8b9327656c6d2eefa402f387ee4
bihlink <95501288+bihlink@users.noreply.github.com>
2026-05-23 08:39:57 +0200

    Add multiple card components to index.php

--- index.php
+++ index.php
@@ -26,6 +26,133 @@
             </div>
         </div>
 
+<!-- start dir -->
+  <div class="row row-cols-1 row-cols-md-4 g-3">
+
+      <div class="col">
+        <div class="card">
+          <img src="https://www.seoclerks.com/pics/t/000/254/580/7b491205c6804374aaa3b6d0699e1291.png" class="card-img-top" alt="DA90 Backlink">
+          <div class="card-body">
+            <a href="https://www.seoclerks.com/Link-Building/503216/3-DA90-Backlinks-Rank-1-on-Google-FAST" target="_blank">
+            <h5 class="card-title">Add Link Here</h5>
+            </a>
+          </div>
+        </div>
+     </div>
+      
+      <div class="col">
+        <div class="card">
+          <img src="https://api.pikwy.com/web/6a1027827cd38d7348727cd5.jpg" class="card-img-top" alt="Visoko">
+          <div class="card-body">
+            <a href="https://visoko.net" target="_blank">
+            <h5 class="card-title">Visoko</h5>
+            </a>
+          </div>
+        </div>
+     </div>
+      
+      <div class="col">
+        <div class="card">
+          <img src="https://api.pikwy.com/web/6a10281efc55b238bf09dd6d.jpg" class="card-img-top" alt="BiH Link">
+          <div class="card-body">
+            <a href="https://bihlink.com" target="_blank">
+            <h5 class="card-title">BiH Link</h5>
+            </a>
+          </div>
+        </div>
+     </div>
+      
+     <div class="col">
+        <div class="card">
+          <img src="https://api.pikwy.com/web/6a1029a1e9ff53079a298311.jpg" class="card-img-top" alt="E7POTA">
+          <div class="card-body">
+            <a href="https://e7pota.ba" target="_blank">
+            <h5 class="card-title">E7POTA</h5>
+            </a>
+          </div>
+        </div>
+     </div>
+
+     <div class="col">
+        <div class="card">
+          <img src="https://api.pagepeeker.com/v2/thumbs.php?size=x&code=e29fa6bbd0&url=https://svxlink.online" class="card-img-top" alt="SVXLink Bosnia">
+          <div class="card-body">
+            <a href="https://svxlink.online/" target="_blank">
+            <h5 class="card-title">SVX Link Bosnia</h5>
+            </a>
+          </div>
+        </div>
+    </div>
+      
+     <div class="col">
+        <div class="card">
+          <img src="https://api.pikwy.com/web/6a10264b8807373be93b61f6.jpg" class="card-img-top" alt="Abid Nevesinjac">
+          <div class="card-body">
+            <a href="https://xabid.com" target="_blank">
+            <h5 class="card-title">Abid Nevesinjac</h5>
+            </a>
+          </div>
+        </div>
+    </div>
+      
+    <div class="col">
+        <div class="card">
+          <img src="https://api.pikwy.com/web/6a1029ea4c6018164621647c.jpg" class="card-img-top" alt="FiveM Store">
+          <div class="card-body">
+            <a href="https://fivem-store.com/" target="_blank">
+            <h5 class="card-title">FiveM Store</h5>
+            </a>
+          </div>
+        </div>
+    </div>
+
+    <div class="col">
+      <div class="card">
+        <img src="https://api.pikwy.com/web/6a102a1a6ff6592e6072642f.jpg" class="card-img-top" alt="Pensivly">
+        <div class="card-body">
+          <a href="https://www.pensivly.com/" target="_blank">
+          <h5 class="card-title">Pensivly</h5>
+          </a>
+        </div>
+      </div>
+    </div>
+
+    <div class="col">
+      <div class="card">
+        <img src="https://api.pikwy.com/web/6a102a543f2237402825ed66.jpg" class="card-img-top" alt="AN Web Services">
+        <div class="card-body">
+          <a href="https://www.seoclerks.com/linkin/254580/user/anwebservices" target="_blank">
+          <h5 class="card-title">AN Web Services</h5>
+          </a>
+        </div>
+      </div>
+    </div>
+
+    <div class="col">
+        <div class="card">
+          <img src="https://free.pagepeeker.com/v2/thumbs.php?size=x&url=seolads.com" class="card-img-top" alt="SEO Services">
+          <div class="card-body">
+            <a href="https://seolads.com" target="_blank">
+            <h5 class="card-title">SEOLADS</h5>
+            </a>
+          </div>
+        </div>
+    </div>
+
+    <div class="col">
+        <div class="card">
+          <img src="https://free.pagepeeker.com/v2/thumbs.php?size=x&url=arabih.ba" class="card-img-top" alt="ARAuBiH">
+          <div class="card-body">
+            <a href="https://arabih.ba" target="_blank">
+            <h5 class="card-title">ARAuBiH</h5>
+            </a>
+          </div>
+        </div>
+    </div>
+
+  </div>
+<!-- end dir -->
+           
        </div>
        <div class="col-md-4 col-lg-3">
          <?php
